b302584d03c47e7198bfe8c896b58ae4ebab5c7f
Md Fahim Hossain <fahimreshad3@gmail.com>
2025-01-24 23:29:57 +0600

    updated

--- app/about/about.tsx
+++ app/about/about.tsx
@@ -4,7 +4,8 @@ const About: React.FC = () => {
     return (
         <div>
             <h1>About Uss</h1>
-            <p>Welcome to our application. This is the About page where you can learn more about us.</p>
+            <p>Welcome to our application. This is the About page where you can learn more about us..</p>
+            <p>Welcome to our application. This is the About page where you can learn more about us..</p>
         </div>
     );
 };
