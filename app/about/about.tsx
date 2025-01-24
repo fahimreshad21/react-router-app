f3cce070e1292776f930fbde80fa5116b3b42a0f
Md Fahim Hossain <fahimreshad3@gmail.com>
2025-01-24 23:28:00 +0600

    updatedd

--- app/about/about.tsx
+++ app/about/about.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const About: React.FC = () => {
     return (
         <div>
-            <h1>About Us</h1>
+            <h1>About Uss</h1>
             <p>Welcome to our application. This is the About page where you can learn more about us.</p>
         </div>
     );
